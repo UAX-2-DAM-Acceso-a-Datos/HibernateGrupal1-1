4a3051fdf848f861a868803b303fac99c9c9d6ce
LAPTOP-L777LCF3\marco <marcosvillenaperez@gmail.com>
2022-11-14 12:19:41 +0100

    Se añade clase vehiculo

---
+++ src/main/java/entities/Vehiculo.java
@@ -0,0 +1,58 @@
+package main.java.entities;
+
+public class Vehiculo {
+
+	String matricula;
+	String marca;
+	String modelo;
+	String revision;
+
+	
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", revision=" + revision
+				+ "]";
+	}
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public String getRevision() {
+		return revision;
+	}
+
+
+	public void setRevision(String revision) {
+		this.revision = revision;
+	}
+	
+	
+	
+}
