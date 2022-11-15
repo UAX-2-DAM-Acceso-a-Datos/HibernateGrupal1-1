be598b1bb009ba4d88d474bfd6eb4feecc8ca04a
Medra235 <102791570+Medra235@users.noreply.github.com>
2022-11-15 10:00:52 +0100

    Clase vehiculo con sus parametros en el main

--- src/main/java/main/VehiculoMain.java
+++ src/main/java/main/VehiculoMain.java
@@ -1,10 +1,23 @@
 package main;
 
+import entities.Vehiculo;
+import utils.HibernateUtils;
+
 public class VehiculoMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new VehiculoMain();
+	}
+	
+	public VehiculoMain() {
+		Vehiculo v = new Vehiculo();
+		v.setMatricula("Goku");
+		v.setMarca("Tesla");
+		v.setModelo("S");
+		
+		
+		HibernateUtils.getSessionFactory().close();
 	}
 
 }
