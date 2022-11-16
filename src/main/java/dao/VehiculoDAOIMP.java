645d7bfe4e6412a6923d2de5e27e2cf92b4b563c
Aitor Nain Mendoza Vallejo <aitornainmendozavallejo@gmail.com>
2022-11-16 07:55:40 +0100

    Agregamos clases implement

---
+++ src/main/java/dao/VehiculoDAOIMP.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.List;
+
+import entities.Vehiculo;
+import idao.IVehiculoDAO;
+
+public class VehiculoDAOIMP implements IVehiculoDAO {
+
+	@Override
+	public boolean addVehiculo(String user, String password, String token) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificarVehiculo(String user, String password, String token) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteVehiculo(String user, String password, String token) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Vehiculo> listarVehiculo(String user, String password, String token) {
+		// TODO Auto-generated method stub
+		return null;
+	} 
+		
+}
