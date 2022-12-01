4d3f229a766a78319cb7eabfc35552c6dd00f5a8
Marcos <marcosvillenaperez@gmail.com>
2022-12-01 11:39:03 +0100

    Test equals y not null

--- src/test/TestVehiculo.java
+++ src/test/TestVehiculo.java
@@ -18,5 +18,36 @@ class TestPruebaCasos {
 	void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testEquals() {
+	Set<String> set1 = new HashSet<String>();
+	set1.add(Vehiculo1);
+	set1.add(Vehiculo2);
+	set1.add(Vehiculo3);
+	Set<String> set2 = new HashSet<String>();
+	assertFalse(set1.equals(set2));
+	set2.add(Vehiculo1);
+	assertFalse(set1.equals(set2));
+	set2.add(Vehiculo2);
+	assertFalse(set1.equals(set2));
+	set2.add(Vehiculo3);
+	assertTrue(set1.equals(set2));
+	assertTrue(set2.equals(set1));
+	
+	}
+	
+	@Test
+	public void testNotNull() {
+	Set<String> set = new HashSet<String>();
+	assertEquals(0, set.size());
+	assertTrue(set.add(Vehiculo1));
+	assertEquals(1, set.size());
+	assertTrue(set.add(Vehiculo2));
+	assertEquals(2, set.size());
+	assertFalse(set.add(Vehiculo3));
+	assertEquals(2, set.size());
+	}
+
 
 }
